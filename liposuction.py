8fbc3e0a1edaf62518d2da1155594ad60fca33aa
Arturo Escudero <darkturo@gmail.com>
2014-03-27 01:18:29 +0100

    Use argparse instead of the deprecated optparse.

--- liposuction.py
+++ liposuction.py
@@ -1,39 +1,35 @@
 #!/usr/bin/python
-from optparse import OptionParser 
+import argparse
 
 #file = open('newfile.txt', 'r')
 #print file.readline():
 #print "LIPOSUCTION"
 
+def liposuction(tfile, includes, dryRun):
+   pass;
+
 def parseArguments(): 
    # Creating the parser.
-   argumentParser = OptionParser("usage: %prog [options] [ttcn_file(s)]");
+   argumentParser = argparse.ArgumentParser(
+                        description="Perform plastic surgery over fatty TTCN3 files",
+                        epilog="eartesc March-2014");
 
    # Setting up the available Options
-   argumentParser.add_option("-I", action="append", type="string", dest="includes",
-                             "Add directory to the Include search path");
-   argumentParser.add_option("--dry-run", type="boolean", dest="dryRun",
-                             "Execute in dry-run mode, i.e.: not proceeding with real actions.");
+   argumentParser.add_argument("-I", "--include", action="append", type=dir,
+                             help="Add directory to the Include search path");
+   argumentParser.add_argument("--dryRun", action="store_true", default=False, 
+                             help="Execute in dry-run mode, i.e.: not proceeding with real actions.");
+   argumentParser.add_argument("ttcn3_files", metavar='TTCN_File', type=file, 
+                               nargs="+", help="TTCN-3 Files to proceed over.");
 
    # Parsing argv 
-   (options, args) = argumentParser.parse_args();
-
-   ttcn3_files = ();
-   if ( len(args) == 0): 
-      # print error and exit with status 2.
-      argumentParser.error("Need at least one ttcn-3 fatty file to proceed on.");
-   else:
-      for file in args:
-         if (isValidTtcn3File(file)):
-            ttcn3_files.append(file);
-         else:
-            argumentParser.error("Incorrect file: " + file + ". Liposuct only operates on ttcn3 files");
-      options.ttcn3_files = ttcn3_files;
+   options = argumentParser.parse_args();
 
    # returning the options
    return options
 
-
 if (__name__ == '__main__'):
    options = parseArguments();
-   liposuction(options.ttcn3_files, option.includes, options);
+         #if (isValidTtcn3File(file)):
+   for ttcn3File in options.ttcn3_files:
+      liposuction(ttcn3File, options.include, dryRun=options.dryRun);
