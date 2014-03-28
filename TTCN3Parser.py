0124fbfe3a0a3b02d4593ccdd90e7511998b2d98
Arturo Escudero <darkturo@gmail.com>
2014-03-28 07:21:06 +0100

    Referencing the symbol tables. Modified parse method.
    Parse will return now a pair with the AST (list of lists), and the
    Symbol table with the contents after running through the file.

--- TTCN3Parser.py
+++ TTCN3Parser.py
@@ -1,18 +1,27 @@
 from lepl import *;
+import TTCN3SymbolTable;
 
 class TTCN3Parser:
-   def __init__(self):
+   def __init__(self, includeDirs=[]):
+      self.includeDirs = includeDirs;
       self.TTCN3Module = __defineTTCN3Grammar();
+      self.symbolTable = ();
 
    def parse(self, ttcn3File):
-      ''' Will return an AST (represented with lists).'''
-      fileHandle = open(ttcn3_file);
-      self.TTCN3Module.parse_file(fileHandle);
+      ''' 
+      Will return a tuple pair with the resulting AST tree after the parsing,
+      and a symbol table containing all the elements fond during the parsing.
+      '''
+      self.symbolTable = TTCN3SymbolTable();
+      fileHandle = open(ttcn3File);
+      ast = self.TTCN3Module.parse_file(fileHandle);
+      return (ast, self.symbolTable);
 
 
    def __defineTTCN3Grammar();
-      ''' Defines the grammar with lepl, and returns the root element, which
-          will be used later to parse a given file.
+      ''' 
+      Defines the grammar with lepl, and returns the root element, which
+      will be used later to parse a given file.
       '''
       TTCN3Module = String();
       return TTCN3Module;
