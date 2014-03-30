d9116a288d573e27846ee61389a17943480f392a
Arturo Escudero <darkturo@gmail.com>
2014-03-30 05:39:47 +0200

    Partial support for the select/case ttcn3 statement
    I'm thinking now that it may be good to try pyparse. I kind of like
    lepl, but I see some limitations on the error handling (or perhaps I
    can't see the idea that its creator had in mind).
    I'm also having a hard time ignoring the new lines between different
    sentences. I have tried a lot of ideas but nothing.
    So in the end, what I'm able to parse is just:
     'select(expression) { case (inlineTemplate) example_block }'
    
    ... I almost forgot, in this commit, I changed again the order of the
    grammar productions.

--- lab/grammar_evolution_test.py
+++ lab/grammar_evolution_test.py
@@ -4,8 +4,5 @@ import grammar_evolution;
 #   def test_assignment(self):
 #           
 
-#grammar_evolution.parse("hit, hit,  hit  , hit");
-#grammar_evolution.parse("heat");
-#grammar_evolution.parse("heat , heat, heat,heat,heat");
-grammar_evolution.parse("heat . heat");
-#grammar_evolution.parse("heat . heat");
+code='''select(expression) { case (inlineTemplate) example_block }'''
+grammar_evolution.parse(code);
